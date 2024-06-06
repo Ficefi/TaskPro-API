b6bf2146fc0e5b11d87af88d07df5366b37e3c54
IvanBuzin <ua.buzin@gmail.com>
2024-06-06 14:38:58 +0300

    model

---
+++ model/tasks.js
@@ -0,0 +1,70 @@
+import { Schema, model } from 'mongoose';
+import Joi from 'joi';
+import { handleSaveError, preUpdate } from './hooks.js';
+
+const priorityList = ['Without', 'Low', 'Medium', 'High'];
+
+const taskScheme = new Schema(
+  {
+    title: {
+      type: String,
+      required: [true, 'Set title for task'],
+    },
+
+    description: {
+      type: String,
+      default: '',
+    },
+
+    priority: {
+      type: String,
+      enum: priorityList,
+      default: 'Without',
+    },
+
+    deadline: {
+      type: String,
+    },
+
+    columnID: {
+      type: Schema.Types.ObjectId,
+      ref: 'column',
+      required: true,
+    },
+
+    owner: {
+      type: Schema.Types.ObjectId,
+      ref: 'user',
+      required: true,
+    },
+  },
+  { versionKey: false, timestamps: false }
+);
+
+taskScheme.post('save', handleSaveError);
+taskScheme.pre('findOneAndUpdate', preUpdate);
+
+export const taskAddSchema = Joi.object({
+  title: Joi.string().max(32).required(),
+  description: Joi.string().max(88).allow(''),
+  priority: Joi.string().valid(...priorityList),
+  deadline: Joi.string(),
+});
+
+export const taskEditSchema = Joi.object({
+  title: Joi.string().max(32).allow('').optional(),
+  description: Joi.string().max(88).allow('').optional(),
+  priority: Joi.string()
+    .valid(...priorityList)
+    .optional(),
+  deadline: Joi.string().optional(),
+  columnID: Joi.string().optional(),
+});
+
+export const taskChangeColumnSchema = Joi.object({
+  columnID: Joi.string().required(),
+});
+
+const Task = model('task', taskScheme);
+
+export default Task;
--- model/user.js
+++ model/user.js
@@ -1,5 +1,10 @@
 import { Schema, model } from 'mongoose';
 import bcrypt from 'bcryptjs';
+import Joi from 'joi';
+import { handleSaveError, preUpdate } from './hook';
+
+const emailPattern = /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/;
+const themeList = ['light', 'violet', 'dark'];
 
 const userSchema = new Schema(
   {
@@ -9,11 +14,13 @@ const userSchema = new Schema(
     },
     email: {
       type: String,
+      match: emailPattern,
       required: [true, 'Email is required'],
       unique: true,
     },
     password: {
       type: String,
+      minlength: 6,
       required: [true, 'Password is required'],
     },
     avatarURL: String,
@@ -25,12 +32,50 @@ const userSchema = new Schema(
       type: String,
       default: null,
     },
+    theme: {
+      type: String,
+      enum: themeList,
+      default: 'violet',
+    },
+    avatar: String,
+    token: String,
   },
-  { versionKey: false }
+  { versionKey: false, timestamps: true }
 );
 
-userSchema.methods.hashPasswords = async function () {
+userScheme.methods.hashPasswords = async function () {
   this.password = await bcrypt.hash(this.password, 10);
 };
 
+userScheme.post('save', handleSaveError);
+userScheme.pre('findOneAndUpdate', preUpdate);
+userScheme.post('findOneAndUpdate', handleSaveError);
+
+export const userSignupScheme = Joi.object({
+  name: Joi.string(),
+  email: Joi.string().pattern(emailPattern),
+  password: Joi.string().min(6),
+});
+
+export const userSigninScheme = Joi.object({
+  email: Joi.string().pattern(emailPattern),
+  password: Joi.string().min(6),
+});
+
+export const userEditScheme = Joi.object({
+  name: Joi.string().allow('').optional(),
+  email: Joi.string().pattern(emailPattern).allow('').optional(),
+  password: Joi.string().min(6).allow('').optional(),
+});
+
+export const userChangeThemeSchema = Joi.object({
+  theme: Joi.string().valid(...themeList),
+});
+
+export const userHelpMailScheme = Joi.object({
+  email: Joi.string(),
+  comment: Joi.string(),
+});
+
 export const User = model('user', userSchema);
+export default User;
